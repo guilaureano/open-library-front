f6c80dfdad9fb7a906b59757d9df435389d25579
guilherme laureano <guilaureano.dev@gmail.com>
2026-05-20 21:46:07 -0300

    feat: add initial tests for useBooks hook to verify book loading functionality

---
+++ src/features/books/hooks/__test__/useBooks.test.tsx
@@ -0,0 +1,24 @@
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { renderHook, waitFor } from '@testing-library/react';
+import type { ReactNode } from 'react';
+import { openLibrarySearchMock } from '../../__test__/fixtures/openLibrary';
+import { useBooks } from '../useBooks';
+
+function wrapper({ children }: { children: ReactNode }) {
+  const client = new QueryClient();
+
+  return <QueryClientProvider client={client}>{children}</QueryClientProvider>;
+}
+
+describe('useBooks', () => {
+  it('loads books', async () => {
+    const { result } = renderHook(() => useBooks({ query: 'harry' }), {
+      wrapper,
+    });
+
+    await waitFor(() => expect(result.current.isSuccess).toBe(true));
+    expect(result.current.data?.docs[0].title).toBe(
+      openLibrarySearchMock.docs[0].title,
+    );
+  });
+});
